d3688f236f4a691524c8f90237985c9c2feb7070
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-28 09:45:10 -0300

    feat: add type constraints (generic types interfaces)

--- generics/add.go
+++ generics/add.go
@@ -11,6 +11,18 @@ func add[T int | float64](args ...T) T {
 	return total
 }
 
+// Generics Constrait
+type NumbersTypes interface {
+	float64 | int
+}
+func addV2[T NumbersTypes](args ...T) T {
+	var total T
+	for _, v := range(args) {
+		total += v
+	}
+	return total
+}
+
 
 func Main() {
 	floatResult := add(1.23, 13.14, 6.485)
@@ -22,5 +34,16 @@ func Main() {
 	sliceParams := []int{10, 20, 30}
 	sliceParamsResult := add(sliceParams...)
 	fmt.Printf("Result of the sum of slice parameters: %d\n", sliceParamsResult)
+
+
+	floatResultV2 := addV2(1.23, 13.14, 6.485)
+	fmt.Printf("Result of the sum of float parameters (version 2): %f\n", floatResultV2)
+
+	integerResultV2 := addV2(1, 2, 3, 4, 5)
+	fmt.Printf("Result of the sum of integer parameters (version 2): %d\n", integerResultV2)
+
+	sliceParams2 := []int{-1, -7, 20}
+	sliceParamsResultV2 := addV2(sliceParams2...)
+	fmt.Printf("Result of the sum of slice parameters (version 2): %d\n", sliceParamsResultV2)
 
 }
