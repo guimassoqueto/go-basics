491c091b17051d044779ccfe6405ef6be35bc757
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-28 09:31:32 -0300

    feat: generics

---
+++ generics/add.go
@@ -0,0 +1,21 @@
+package generics
+
+import "fmt"
+
+
+func add[T int | float64](args ...T) T {
+	var total T
+	for _, v := range(args) {
+		total += v
+	}
+	return total
+}
+
+
+func Main() {
+	floatResult := add(1.23, 13.14, 6.485)
+	fmt.Printf("Result of the sum of float parameters: %f\n", floatResult)
+
+	integerResult := add(1, 2, 3, 4, 5)
+	fmt.Printf("Result of the sum of integer parameters: %d\n", integerResult)
+}
